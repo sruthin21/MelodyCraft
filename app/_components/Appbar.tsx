f42861507e990128e7c0341230c934057238be1c
sruthin21 <sruthin.rayella@gmail.com>
2024-12-01 01:47:40 +0530

    Created Streams Post and GET Request

--- app/_components/Appbar.tsx
+++ app/_components/Appbar.tsx
@@ -7,7 +7,7 @@ const Appbar = () => {
   return (
     <div className='flex justify-between'>
       <div>
-        Muzi
+        Music
       </div>
       <div>
         {session.data?.user && <button className='m-2 p-2 bg-blue-400' onClick={()=> signOut()}>Logout</button>  }
